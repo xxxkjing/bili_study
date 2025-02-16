26504f4e4798c64ba8c22f12e29a1a16f2b34887
MinusTen <xkjing@outlook.com>
2025-02-16 11:07:45 +0800

    Update [id].tsx

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import type { NextPage } from 'next'
-import type { ReactElement } from 'react'
+import React, { type ReactElement } from 'react'
 import Head from 'next/head'
 
 // 定义组件接口
